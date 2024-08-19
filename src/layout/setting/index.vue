20728fa0c701db4ca5204da7a8b9b8efeed81508
tongmiao <tongmiao@aicaihr.com>
2024-08-19 15:43:47 +0800

    修改主题颜色

--- src/layout/setting/index.vue
+++ src/layout/setting/index.vue
@@ -1,29 +1,49 @@
 <template>
-    <el-drawer v-model="settingStore.settingsVisible" title="项目配置" direction="rtl" >
+    <el-drawer v-model="settingStore.settingsVisible" title="项目配置" direction="rtl">
         <el-divider content-position="center">主题设置</el-divider>
         <div class="common-flex justify-center">
             <el-switch v-model="themeDefault" @change="themeChange" active-icon="Moon" inactive-icon="Sunny" />
         </div>
         <el-divider content-position="center">界面设置</el-divider>
+        <div class="common-flex justify-between">
+            <span>主题颜色</span>
+            <el-color-picker v-model="color" :predefine="predefineColors" @change="changeThemeColor" />
+        </div>
         <el-divider content-position="center">导航设置</el-divider>
     </el-drawer>
 </template>
 
 <script setup lang='ts'>
-import {useSettingStore} from '@/store'
+import { useSettingStore } from '@/store'
 
 let settingStore = useSettingStore()
 let themeDefault = ref(false)   //主题 false默认模式，true为暗黑模式
+const color = ref()
+const predefineColors = ref([
+    "#409EFF",
+    "#ff4500",
+    "#ff8c00",
+    "#90ee90",
+    "#00ced1",
+    "#1e90ff",
+    "#c71585",
+    "rgba(255, 69, 0, 0.68)",
+    "rgb(255, 120, 0)",
+    "hsva(120, 40, 94)",
+])
 
-let themeChange = ()=>{
+let themeChange = () => {
     settingStore.themeChange()
 }
-
-onMounted(()=>{
+let changeThemeColor = () => {
+    settingStore.changeThemeColor(color.value)
+}
+onMounted(() => {
     let scheme = localStorage.getItem('vueuse-color-scheme')
-    if(scheme =='dark'){
+    if (scheme == 'dark') {
         themeDefault.value = true
     }
+    color.value = settingStore.themeColor
 })
 </script>
 
--- src/store/modules/setting.ts
+++ src/store/modules/setting.ts
@@ -1,13 +1,31 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import { useDark, useToggle } from '@vueuse/core'
+import { useDark, useStorage, useToggle } from '@vueuse/core'
+import Color from "color";
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 
 export const useSettingStore = defineStore("setting",()=>{
-    //设置弹窗
+    //是否显示设置
     let settingsVisible = ref(false)
+    // 主题颜色
+    let themeColor = ref((JSON.parse(localStorage.getItem('setting')||'{}').themeColor) || '#409EFF' )
+    
+    watch(themeColor,(newThemeColor,oldThemeColor)=>{
+        if(newThemeColor!=oldThemeColor){
+            document.documentElement.style.setProperty('--el-color-primary', newThemeColor);
+            document.documentElement.style.setProperty('--el-color-primary-dark-2', newThemeColor);
+            for (let i = 1; i < 10; i++) {
+                document.documentElement.style.setProperty(
+                  `--el-color-primary-light-${i}`,
+                  `${Color(newThemeColor).alpha(1 - i * 0.1)}`
+                );
+              }
+        }
+    },{
+        immediate:true
+    })
     //打开设置弹窗
     function openSetting(){
         settingsVisible.value = true
@@ -16,6 +34,16 @@ export const useSettingStore = defineStore("setting",()=>{
     function themeChange(){
         toggleDark()
     }
-    return {settingsVisible,openSetting,themeChange}
+    //切换主题颜色
+    function changeThemeColor(color:string){
+        themeColor.value = color
+    }
+    return {
+        settingsVisible,
+        themeColor,
+        openSetting,
+        themeChange,
+        changeThemeColor,
+    }
 },
 {persist:true})
