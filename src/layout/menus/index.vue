dccb54337c1da1469706fcfc8fa241ec9557b74a
tongmiao <tongmiao@aicaihr.com>
2024-08-21 10:55:23 +0800

    menu-logo

--- src/layout/menus/index.vue
+++ src/layout/menus/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <el-menu
-    :background-color="variables['menu-background']"
-    :text-color="variables['menu-text']"
-    :active-text-color="variables['menu-active-text']"
-    class="el-menu-layout"
-    :default-active="defaultActive"
-    :router="true"
-    :collapse="!appStore.sidebarStatus"
-  >
+  <el-menu :default-active="defaultActive" :background-color="variables['menu-background']"
+    :text-color="variables['menu-text']" :active-text-color="variables['menu-active-text']" class="el-menu-layout"
+    :router="true" :collapse="!appStore.sidebarStatus">
+    <el-menu-item index="/dashboard" class="logo-container">
+      <img src="@/assets/logo.png" alt="" class="logo">
+      <span class="logo-title">vue3-element-admin</span>
+    </el-menu-item>
     <el-menu-item index="/dashboard">
-     <i class="el-icon"><svg-icon icon-class="homepage" /></i>
+      <i class="el-icon"><svg-icon icon-class="homepage" /></i>
       <span>首页</span>
     </el-menu-item>
     <menuList :menus="menus"></menuList>
@@ -20,7 +18,6 @@
 import { useUserStore } from "@/store/index";
 import menuList from "./menuList.vue";
 import { useAppStore } from "@/store";
-
 import variables from '@/styles/variables.module.scss'
 
 let userStore = useUserStore();
@@ -38,5 +35,23 @@ onMounted(async () => {
 </script>
 
 <style scoped lang="scss">
+.logo-container {
+  background-color: var(--sidebar-logo-background);
+  .logo {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+  }
 
+  .logo-title {
+    color: #fff;
+    font-weight: bold;
+  }
+  .logo-title:hover{
+    color: pink;
+  }
+}
+.el-menu-item:hover {
+  background-color: var(--menu-hover);
+}
 </style>
--- src/layout/menus/menuList.vue
+++ src/layout/menus/menuList.vue
@@ -1,11 +1,11 @@
 <template>
   <el-sub-menu :index="item.path" v-for="item in menus">
     <template #title>
-     <sidebarTitle :icon="item.meta.icon" :title="item.meta.title"  />
+      <sidebarTitle :icon="item.meta.icon" :title="item.meta.title" />
     </template>
     <div v-if="item.children.length !== 0">
       <el-menu-item v-for="child in item.children" :index="child.meta.fullPath">
-        <sidebarTitle :icon="child.meta.icon" :title="child.meta.title"  />
+        <sidebarTitle :icon="child.meta.icon" :title="child.meta.title" />
       </el-menu-item>
     </div>
   </el-sub-menu>
@@ -15,20 +15,28 @@
 import sidebarTitle from './sidebarTitle.vue'
 
 interface meta {
-    path:string,
-    meta:{
-        title:string,
-        icon:string,
-        fullPath:string
-    },
+  path: string,
+  meta: {
+    title: string,
+    icon: string,
+    fullPath: string
+  },
 }
 interface Item extends meta {
-    children:meta[]
+  children: meta[]
 }
 let props = defineProps({
-    menus:Array<Item>
+  menus: Array<Item>
 })
 </script>
 
 <style scoped lang="scss">
+.el-sub-menu{
+  :deep(.el-sub-menu__title:hover) {
+    background-color: var(--menu-hover);
+  }
+}
+.el-menu-item:hover {
+  background-color: var(--menu-hover);
+}
 </style>
